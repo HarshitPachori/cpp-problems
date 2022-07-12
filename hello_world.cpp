e575c545e183d7403d2ed2273c32ed4786a78538
Harshit Pachori <harshitpachori345@gmail.com>
2022-07-12 15:35:20 +0530

    hello_world program

---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+//! This is our first program in C++.
+#include <iostream>
+using namespace std;
+
+main()
+{
+    cout << "Hello World!" << endl; // cout is a function that prints to the screen.
+}
