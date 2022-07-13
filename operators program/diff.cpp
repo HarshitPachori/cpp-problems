587f36ed9638adf4471b5302175c05981012c3c7
Harshit Pachori <harshitpachori345@gmail.com>
2022-07-13 08:52:03 +0530

    difference of two numbers

---
+++ operators program/diff.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int n1, n2;
+    cout << "Enter two numbers: " << endl;
+    cin >> n1 >> n2;
+    int diff = n1 - n2;
+    cout << "Difference of two numbers is: " << diff << endl;
+}
