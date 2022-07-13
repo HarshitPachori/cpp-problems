3c5aa830816ffccb6105c7069aa18b1d25c1e37c
Harshit Pachori <harshitpachori345@gmail.com>
2022-07-13 08:53:04 +0530

    multiplication of two numbers

---
+++ operators program/mul.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int n1, n2;
+    cout << "Enter two numbers: " << endl;
+    cin >> n1 >> n2;
+    int mul = n1 * n2;
+    cout << "Multiplication of two numbers is: " << mul << endl;
+}
