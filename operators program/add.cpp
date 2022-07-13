1b974c2984ec8cb6ccfc5efbfaa4fb086d5ef609
Harshit Pachori <harshitpachori345@gmail.com>
2022-07-13 08:50:53 +0530

    sum of two numbers

---
+++ operators program/add.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int n1, n2;
+    cout << "Enter two numbers: " << endl;
+    cin >> n1 >> n2;
+    int sum = n1 + n2;
+    cout << "Sum of two numbers is: " << sum << endl;
+}
