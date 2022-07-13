8f372dd419b49b8cf6ad926cad113530db114cd4
Harshit Pachori <harshitpachori345@gmail.com>
2022-07-13 09:00:54 +0530

    division of two numbers

---
+++ operators program/div.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int n1, n2;
+    cout << "Enter two numbers: " << endl;
+    cin >> n1 >> n2;
+    int div = n1 / n2;
+
+    cout << "Division of two numbers is: " << div << endl;
+}
