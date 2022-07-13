f599b89f69a9b2324bfd3721fb0f7f86898cda99
Harshit Pachori <harshitpachori345@gmail.com>
2022-07-13 07:53:33 +0530

    if condition

---
+++ conditions programs/if_condition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number:" << endl;
+    cin >> n;
+    if (n > 0)
+    {
+        cout << "You entered a positive number" << endl;
+    }
+    cout << "The number is: " << n << endl;
+    return 0;
+}
